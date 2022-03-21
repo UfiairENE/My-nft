36775a28fb7d12ca75cdf7cb9536a0b98b2ed690
UfiairENE <irenecufia@gmail.com>
2022-03-21 12:24:10 +0100

    'fixed'

---
+++ scripts/mint-nft.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+const API_URL = process.env.API_URL;
+const PUBLIC_KEY = process.env.PUBLIC_KEY;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
+const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
+const web3 = createAlchemyWeb3(API_URL);
+
+const contract = require("../artifacts/contracts/MyNFT.sol/MyNFT.json");
+const contractAddress = "0x6136186a8674fbb12470f167a8820d39c3fe301f";
+const nftContract = new web3.eth.Contract(contract.abi, contractAddress);
+
+async function mintNFT(tokenURI) {
+  const nonce = await web3.eth.getTransactionCount(PUBLIC_KEY, 'latest'); //get latest nonce
+
+  //the transaction
+  const tx = {
+    'from': PUBLIC_KEY,
+    'to': contractAddress,
+    'nonce': nonce,
+    'gas': 500000,
+    'maxPriorityFeePerGas': 2999999987,
+    'data': nftContract.methods.mintNFT(PUBLIC_KEY, tokenURI).encodeABI()
+  };
+
+  const signedTx = await web3.eth.accounts.signTransaction(tx, PRIVATE_KEY);
+  const transactionReceipt = await web3.eth.sendSignedTransaction(signedTx.rawTransaction);
+  
+  console.log(`Transaction receipt: ${JSON.stringify(transactionReceipt)}`);
+}
+
+mintNFT("https://gateway.pinata.cloud/ipfs/QmdoaQzxz99dv2XkDVSo6EGDUScdn2MDSm8m2gBpCwmxvG");
+
+
+
